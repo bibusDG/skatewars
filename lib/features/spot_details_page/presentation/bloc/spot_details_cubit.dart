612a1e025c5d93a3c2298ac43e13dc4601ac8ac3
bibusDG <grosserd81@gmail.com>
2024-03-27 11:15:47 +0100

    spot details page draft done

--- lib/features/spot_details_page/presentation/bloc/spot_details_cubit.dart
+++ lib/features/spot_details_page/presentation/bloc/spot_details_cubit.dart
@@ -1,11 +1,34 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:skatewars/features/add_skate_spot_page/domain/entities/skateSpot.dart';
 
+import '../../domain/usecases/get_spot_details_usecase.dart';
+
 part 'spot_details_state.dart';
 part 'spot_details_cubit.freezed.dart';
 @injectable
 class SpotDetailsCubit extends Cubit<SpotDetailsState> {
-  SpotDetailsCubit() : super(const SpotDetailsState.initial());
+  final GetSpotDetailsUseCase getSpotDetailsUseCase;
+  SpotDetailsCubit({required this.getSpotDetailsUseCase}) : super(const SpotDetailsState.initial());
+
+  StreamSubscription<SkateSpot>? _streamSubscription;
+
+  Future<void> initSpotDetailPage({required String spotID}) async{
+    final result = await getSpotDetailsUseCase(GetSpotDetailsParams(spotID: spotID));
+    result.fold((failure){
+      emit(const SpotDetailsState.spotDetailsPageError(message: 'Upps...Something went wrong'));
+    }, (success){
+      _streamSubscription = success.listen((event) {
+        emit(SpotDetailsState.spotDetailsPageLoaded(skateSpot: event));
+      });
+    });
+  }
+  @override
+  Future<void> close() async{
+    _streamSubscription?.cancel();
+    super.close();
+  }
 }
--- lib/features/spot_details_page/presentation/pages/spot_details_page.dart
+++ lib/features/spot_details_page/presentation/pages/spot_details_page.dart
@@ -1,7 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:hooked_bloc/hooked_bloc.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:skatewars/core/custom_widgets/carousel.dart';
+import 'package:skatewars/features/add_skate_spot_page/domain/entities/skateSpot.dart';
 import 'package:skatewars/features/spot_details_page/presentation/bloc/spot_details_cubit.dart';
+import 'package:collection/collection.dart';
+import 'package:skatewars/features/user_relations/domain/entities/my_user.dart';
 
 class SpotDetailsPage extends HookWidget {
   final String spotID;
@@ -9,17 +20,184 @@ class SpotDetailsPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final _spotDetailsCubit = useBloc<SpotDetailsCubit>();
     final _spotDetailsState = useBlocBuilder(_spotDetailsCubit);
 
+    useEffect(() {
+      _spotDetailsCubit.initSpotDetailPage(spotID: spotID);
+    });
+
     return Scaffold(
-      appBar: AppBar(title: Text('SPOT DETAILS'),),
+      appBar: AppBar(
+        title: Text('SPOT DETAILS'),
+      ),
       body: SizedBox(
         width: double.infinity,
         height: double.infinity,
-        child: Card(color: Colors.deepOrange,)
+        child: _spotDetailsState.whenOrNull(
+          spotDetailsPageLoaded: (skateSpot) => Card(
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: SizedBox(
+                      width: double.infinity,
+                      height: double.infinity,
+                      child: Card(
+                        color: Colors.black,
+                        child: CustomCarouselSlider(gallery: skateSpot.spotPhotos,),
+                      )),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                          flex: 2,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.comment,
+                              size: 40,
+                            ),
+                            color: Colors.teal,
+                          )),
+                      Expanded(
+                          flex: 6,
+                          child: Center(
+                              child: Text(
+                            skateSpot.spotName.toUpperCase(),
+                            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w300),
+                          ))),
+                      Expanded(
+                          flex: 2,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.star_rate_outlined,
+                              size: 40,
+                            ),
+                            color: Colors.teal,
+                          ))
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: RatingBarIndicator(
+                    itemBuilder: (context, index) => const Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    rating: skateSpot.spotRanks.sum / skateSpot.spotRanks.length,
+                    itemCount: 5,
+                    itemSize: 40,
+                  ),
+                ),
+                Expanded(
+                    flex: 5,
+                    child: Row(
+                      children: [
+                        Expanded(
+                            flex: 4,
+                            child: SizedBox(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: Card(
+                                  clipBehavior: Clip.antiAlias,
+                                  child: SpotDetailMap(skateSpot: skateSpot,),
+                                ))),
+                        Expanded(
+                            flex: 3,
+                            child: Stack(
+                              alignment: AlignmentDirectional.topEnd,
+                              children: [
+                                SizedBox(
+                                    width: double.infinity,
+                                    height: double.infinity,
+                                    child: Card(
+                                      child: ListView.builder(
+                                        itemCount: skateSpot.spotRiders.length,
+                                          itemExtent: 80,
+                                          itemBuilder: (BuildContext context, int index){
+                                        return Card(child:
+                                          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                                            CircleAvatar(),
+                                            Text('DG')
+                                          ],),);
+                                      }),
+                                      color: Colors.purple,
+                                    )),
+                                CircleAvatar(child: Text(skateSpot.spotRiders.length.toString())),
+                              ],
+                            )),
+                      ],
+                    )),
+                Expanded(flex: 1, child: Row(
+                  children: [
+                    Expanded(
+                      flex:7,
+                      child: ListView.builder(
+                          itemExtent: 120,
+                          itemCount: skateSpot.spotProperties.length,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (BuildContext context, int index){
+                            return Card(
+                              color: Colors.teal,
+                              child: Center(child: Text(skateSpot.spotProperties[index])),
+                            );
+                          }),
+                    ),
+                    Expanded(flex: 4, child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("  I'm here "),
+                        CupertinoSwitch(value: true, onChanged: (bool value) {  },),
+                      ],
+                    ))
+                  ],
+                ),),
+              ],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class SpotDetailMap extends StatelessWidget {
+  final SkateSpot skateSpot;
+  const SpotDetailMap({
+    super.key,
+    required this.skateSpot
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: MapOptions(
+        initialZoom: 15.0,
+        initialCenter: LatLng(double.parse(skateSpot.spotLat), double.parse(skateSpot.spotLang)),
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        MarkerLayer(markers: [Marker(
+          width: 80,
+          height: 80,
+          point: LatLng(double.parse(skateSpot.spotLat), double.parse(skateSpot.spotLang)),
+          rotate: false,
+          child: const Icon(
+            Icons.location_pin,
+            color: Colors.red,
+            size: 50,
+          ),
+        ),]),
+      ],
     );
   }
 }
